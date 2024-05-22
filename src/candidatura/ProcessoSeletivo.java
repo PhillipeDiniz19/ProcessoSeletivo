0a74fcc8a781043d9435dc532337271093fee42a
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-22 10:40:37 -0300

    seleção de condidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,35 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
   public static void main(String[] args) {
-    analisarCandidato(1900.0);
-    analisarCandidato(2200.0);
-    analisarCandidato(2000.0);
+    // analisarCandidato(1900.0);
+    selecaoCandidato(); 
+  }
 
+  static void selecaoCandidato(){
+    String [] candidatos = {"Phillipe", "Marcos", "Luiz", "Fernando", "Pedro", "Arthur", "Gabriel", "João", "Diniz", "Cardoso"} ;
+    int candidatosSelecionados = 0;
+    int candidatoAtual = 0;
+    double salarioBase = 2000.0;
+    while(candidatosSelecionados < 5){
+      String candidato = candidatos[candidatoAtual];
+      double salarioPretendido = valorPretendido();
+
+      System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+      if(salarioBase >= salarioPretendido ){
+        System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+        candidatosSelecionados++;
+      }else{
+        System.out.println("O candidato não foi selecionado.");
+      }
+      candidatoAtual++;
+    }
+  }
+
+  static double valorPretendido() {
+    return ThreadLocalRandom.current().nextDouble(1800, 2200);
   }
 
   static void analisarCandidato(double salarioPretendido){
