d9835317a9474e4305c2b39068c183ad41d9a17b
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-23 12:00:01 -0300

    criando função imprimirSelecionados

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -5,9 +5,18 @@ import java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
   public static void main(String[] args) {
     // analisarCandidato(1900.0);
-    selecaoCandidato(); 
+    imprimirSelecionados(); 
   }
 
+  static void imprimirSelecionados(){
+    String [] candidatos = {"Phillipe", "Marcos", "Luiz", "Fernando", "Pedro", "Arthur", "Gabriel", "João", "Diniz", "Cardoso"} ;
+    System.out.println("Imprimindo a lista de candidatos informando o indece do elemento");
+    for(int indice = 0; indice <= candidatos.length; indice++){
+      System.out.println("O candidato de numero " + indice + " é " + candidatos[indice]);
+    }
+  }
+
+// Função para seleção de candidato 
   static void selecaoCandidato(){
     String [] candidatos = {"Phillipe", "Marcos", "Luiz", "Fernando", "Pedro", "Arthur", "Gabriel", "João", "Diniz", "Cardoso"} ;
     int candidatosSelecionados = 0;
@@ -27,6 +36,8 @@ public class ProcessoSeletivo {
       candidatoAtual++;
     }
   }
+
+//Pegar valores aleatorios entre 1800, 2200
 
   static double valorPretendido() {
     return ThreadLocalRandom.current().nextDouble(1800, 2200);
