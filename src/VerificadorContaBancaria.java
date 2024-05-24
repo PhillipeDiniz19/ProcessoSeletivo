3ae6b6c5b384b4dd9b798f3a3da3dc61aefa8cc4
PhillipeDiniz19 <phillipediniz19@gmail.com>
2024-05-24 15:48:28 -0300

    validação numero da conta

---
+++ src/VerificadorContaBancaria.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class VerificadorContaBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numeroConta = scanner.nextLine();
+
+        try {
+            verificarNumeroConta(numeroConta);
+            System.out.println("Numero de conta valido.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro: Numero de conta invalido. Digite exatamente 8 digitos.");
+        }
+
+        scanner.close();
+    }
+
+    public static void verificarNumeroConta(String numeroConta) {
+        if (numeroConta.length() != 8 || !numeroConta.matches("\\d+")) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
